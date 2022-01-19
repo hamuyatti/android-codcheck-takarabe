01e9f203db666398b2fd7ef2cb140ab5efac2ec3
hamuyatti <syosatci@gmauil.com>
2022-01-19 14:21:02 +0900

    apiパッケージをwebAPiパッケージに変えたときにファイルを移動し忘れていたので移動させた。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/webApi/Api.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/webApi/Api.kt
@@ -1,10 +1,8 @@
-package jp.co.yumemi.android.code_check.api
+package jp.co.yumemi.android.code_check.webApi
 
-import jp.co.yumemi.android.code_check.entity.RepositoryInfo
 import jp.co.yumemi.android.code_check.entity.Search
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface Api {
